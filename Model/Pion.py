b7dbee3cf00b247b64cc369232ae764dfe100606
ylan <ylan.lodi@gmail.com>
2024-01-08 19:18:01 +0100

    Ajout de la fonction detecter4horizontalPlateau

---
+++ Model/Pion.py
@@ -0,0 +1,93 @@
+# Model/Pion.py
+
+from Model.Constantes import *
+
+#
+# Ce fichier implémente les données/fonctions concernant le pion
+# dans le jeu du Puissance 4
+#
+# Un pion est caractérisé par :
+# - sa couleur (const.ROUGE ou const.JAUNE)
+# - un identifiant de type int (pour l'interface graphique)
+#
+# L'identifiant sera initialisé par défaut à None
+#
+
+def type_pion(pion: dict) -> bool:
+    """
+    Détermine si le paramètre peut être ou non un Pion
+
+    :param pion: Paramètre dont on veut savoir si c'est un Pion ou non
+    :return: True si le paramètre correspond à un Pion, False sinon.
+    """
+    return type(pion) == dict and len(pion) == 2 and const.COULEUR in pion.keys() \
+        and const.ID in pion.keys() \
+        and pion[const.COULEUR] in const.COULEURS \
+        and (pion[const.ID] is None or type(pion[const.ID]) == int)
+
+
+def construirePion(numCouleur : int) -> dict:
+    """
+    Fonction qui construit un pion
+    :param numCouleur: numéro de la couleur du pion
+    :return:dictionnaire contenant les informations du pion
+    :raise TypeError: Si le paramètre n’est pas un entier
+    :raise ValueError: Si l’entier ne représente pas une couleur
+    """
+    if type(numCouleur) != int :
+        raise TypeError ("construirePion : Le paramètre n’est pas de type entier")
+    elif numCouleur!=0 and numCouleur!=1 :
+        raise ValueError ("construirePion : la couleur (valeur_du_paramètre) n’est pas correcte")
+    else:
+        if numCouleur == 0 :
+            pion ={'Couleur':const.JAUNE, 'Identifiant':None }
+        else :
+            pion ={'Couleur':const.ROUGE, 'Identifiant':None}
+    return pion
+
+def getCouleurPion(pion:dict)->int :
+    """
+    Fonction qui retourne la couleur du pion
+    :param pion: dictionnaire correspondant à un pion
+    :return: la couleur du pion
+    """
+    if type(pion) != dict :
+        raise TypeError ("getCouleurPion : Le paramètre n’est pas un pion")
+    return pion['Couleur']
+
+def setCouleurPion(pion:dict, numCouleur)->None:
+    """
+    Fonction qui retourne un pion de couleur associé à numCouleur
+    :param pion: dictionnaire correspondant à un pion
+    :param numCouleur: entier correspondant à la couleur du pion
+    :return: None
+    """
+    if type(pion) != dict :
+        raise TypeError ("setCouleurPion : Le premier paramètre n’est pas un pion.")
+    if type(numCouleur) != int:
+        raise TypeError("setCouleurPion : Le second paramètre n’est pas un entier.")
+    if numCouleur != 0 and numCouleur != 1 :
+        raise ValueError (" setCouleurPion : Le second paramètre ", numCouleur," n’est pas une couleur")
+    else :
+        if numCouleur == 0 :
+            pion['Couleur']=const.JAUNE
+        else:
+            pion['Couleur']=const.ROUGE
+    return None
+
+def getIdPion(pion)->int:
+    """
+    Fonction qui retourne l'idenetifiant du pion
+    :param pion: dictionnaire correspondant à un pion
+    :return: l'indentifant du pion ou None si il n'en a pas
+    """
+    if type(pion) != dict :
+        raise TypeError ("getIdPion : Le paramètre n’est pas un pion.")
+    return pion['Identifiant']
+def setIdPion (pion : dict,id : int) -> None :
+    if type(pion) != dict :
+        raise TypeError ("setIdPion : Le premier paramètre n’est pas un pion.")
+    if type(id) != int :
+        raise TypeError (" setIdPion : Le second paramètre n’est pas un entier.")
+    pion['Identifiant'] = id
+    return None
---
+++ Model/Plateau.py
@@ -0,0 +1,117 @@
+from Model.Constantes import *
+from Model.Pion import *
+
+
+#
+# Le plateau représente la grille où sont placés les pions.
+# Il constitue le coeur du jeu car c'est dans ce fichier
+# où vont être programmées toutes les règles du jeu.
+#
+# Un plateau sera simplement une liste de liste.
+# Ce sera en fait une liste de lignes.
+# Les cases du plateau ne pourront contenir que None ou un pion
+#
+# Pour améliorer la "rapidité" du programme, il n'y aura aucun test sur les paramètres.
+# (mais c'est peut-être déjà trop tard car les tests sont fait en amont, ce qui ralentit le programme...)
+#
+
+def type_plateau(plateau: list) -> bool:
+    """
+    Permet de vérifier que le paramètre correspond à un plateau.
+    Renvoie True si c'est le cas, False sinon.
+
+    :param plateau: Objet qu'on veut tester
+    :return: True s'il correspond à un plateau, False sinon
+    """
+    if type(plateau) != list:
+        return False
+    if len(plateau) != const.NB_LINES:
+        return False
+    wrong = "Erreur !"
+    if next((wrong for line in plateau if type(line) != list or len(line) != const.NB_COLUMNS), True) == wrong:
+        return False
+    if next((wrong for line in plateau for c in line if not(c is None) and not type_pion(c)), True) == wrong:
+        return False
+    return True
+
+def construirePlateau()->list :
+    """
+    Fonction qui construit le plateau de jeu
+    :return:
+    """
+    tab=[]
+    ligne=[]
+    #le plateau possède un même nombre de ligne pour chaques colonnes
+    for i in range(const.NB_COLUMNS):
+        ligne.append(None)
+    for i in range(const.NB_LINES):
+        tab.append(ligne)
+    return tab
+
+def placerpinPlateau(plateau : list, pion : dict, numCol :int) -> int :
+    """
+    fonction qui place un pion dans un plateau à une colonnes numCol
+    :param plateau: le plateau de jeu
+    :param pion: le pion à placer
+    :param numCol: numéro de la colonne où lacher le pion
+    :return: le numéro de la ligne où se situe le pion laché, -1 si impossible
+    """
+    numLigne=-1
+    i=0
+    if type(plateau)!=list :
+        raise TypeError ("placerPionPlateau : Le premier paramètre ne correspond pas à un plateau")
+    if type(pion)!=dict:
+        raise TypeError ("placerPionPlateau : Le second paramètre n’est pas un pion")
+    if type(numCol)!=int :
+        raise TypeError ("placerPionPlateau : Le troisième paramètre n’est pas un entier")
+    if numCol < 0 or numCol >const.NB_COLUMNS :
+        raise ValueError (f" placerPionPlateau : La valeur de la colonne {numCol} n’est pas correcte")
+    while const.NB_LINES-i >0 and numLigne == -1:
+        if plateau[const.NB_LINES-i][numCol] == None:
+            numLigne=const.NB_LINES-i
+        i+=1
+    return numLigne
+
+def detecter4horizontalPlateau(plateau, numCoul) -> list:
+    """
+
+    :param plateau:
+    :param numCoul:
+    :return:
+    """
+    aligne = False
+    i=0
+    res=[]
+    if type(plateau) != list :
+        raise TypeError ("detecter4horizontalPlateau : Le premier paramètre ne correspond pas à un plateau")
+    if type(numCoul) != int :
+        raise TypeError ("detecter4horizontalPlateau : le second paramètre n’est pas un entier")
+    if numCoul != 0 and numCoul!=1 :
+        raise ValueError ("détecter4horizontalPlateau : La valeur de la couleur ",numCoul," n’est pas correcte")
+    for ligne in range(len(plateau)):
+        aligne=False
+        i=0
+        while i < const.NB_COLUMNS-3 and aligne == False :
+            print(ligne + i, ligne, i, type(plateau[ligne][i]), plateau[ligne][i])
+            #print(ligne,i,type(plateau[ligne][i]),i)
+            if plateau[ligne][i]!=None and plateau[ligne][i+1]!=None and plateau[ligne][i+2]!=None and plateau[ligne][i+3]!=None and getCouleurPion(plateau[ligne][i]) == numCoul and getCouleurPion(plateau[ligne][i+1]) == getCouleurPion(plateau[ligne][i]) and getCouleurPion(plateau[ligne][i+1]) == getCouleurPion(plateau[ligne][i+2]) and getCouleurPion(plateau[ligne][i+2]) == getCouleurPion(plateau[ligne][i+3]):
+                aligne=True
+                for u in range (4):
+                    res.append(plateau[ligne][i+u])
+            i+=1
+    return res
+
+
+
+
+
+
+#test 4 horizontal
+pion={'Couleur':const.JAUNE,'id':None}
+plat=[[None,None,None,{'Couleur':const.JAUNE,'id':1},{'Couleur':const.JAUNE,'id':2},{'Couleur':const.JAUNE,'id':3},{'Couleur':const.JAUNE,'id':4}],[None,None,None,None,None,None,{'Couleur':const.JAUNE,'id':7}],[None,None,None,None,None,None,{'Couleur':const.JAUNE,'id':8}],[None,None,None,None,None,None,{'Couleur':const.JAUNE,'id':9}],[None,None,None,None,None,None,{'Couleur':const.JAUNE,'id':10}],[None,None,None,None,None,None,{'Couleur':const.JAUNE,'id':11}]]
+print(detecter4horizontalPlateau(plat,0))
+
+
+
+
+
